ef26c497c4a5d44a60268d1bef79880f1ccf5e4e
Nurzhigit <152197662+Nurzhigit08@users.noreply.github.com>
2024-04-01 16:16:49 +0500

    Add files via upload

---
+++ ToDoList/java.js
@@ -0,0 +1,46 @@
+let button = document.getElementById('button');
+let list = document.getElementById('ul');
+
+button.addEventListener('click', function (event) {
+    event.preventDefault();
+    
+    if (document.getElementById('input').value !== '') {
+        let text = document.getElementById('input').value;
+        let item = document.createElement('span');
+        item.classList.add('item');
+        
+        item.innerHTML = `
+            <span id='task'>
+                <span id='item'>
+                    <input type='checkbox' class='check'/> <!-- Changed id to class -->
+                    <label>${text}</label>
+                </span>
+                <span>
+                    <button class='btn-del'>del</button> <!-- Changed id to class -->
+                </span>
+            </span>`
+        
+        list.appendChild(item);
+    }
+    
+    document.getElementById('input').value = '';
+});
+
+list.addEventListener('click', function(event) {
+    if (event.target.classList.contains('btn-del')) {
+        event.target.parentElement.parentElement.remove();
+    } else if (event.target.classList.contains('check')) { 
+        if (event.target.checked) {
+            let taskText = event.target.nextElementSibling.textContent;
+            addToTable(taskText);
+            event.target.parentElement.parentElement.remove();
+        }
+    }
+});
+
+function addToTable(task) {
+    let tableBody = document.getElementById('infoTableBody');
+    let newRow = tableBody.insertRow();
+    let cell = newRow.insertCell(0);
+    cell.innerHTML = task;
+}
